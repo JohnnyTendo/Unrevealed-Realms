d17f70be3ee3a7eb0e13515dc38d265f35da4892
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-10-28 14:10:07 +0100

    Added Items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,10 +7,31 @@ public class Inventory : MonoBehaviour
     [SerializeField]
     GameObject prefab;
     public Canvas inventoryUi;
+    
+    //New
+    Item[] items;
+    //EndNew
 
     public void SetUp()
     {
         GameObject holder = Instantiate(prefab);
         inventoryUi = holder.GetComponent<Canvas>();
+        items = new Item[10]();
+    }
+    
+    public Item GetItem(int _index)
+    {
+        return items[_index];
+    }
+    public string SetItem(Item _item)
+    {
+        for(int i = 0; i <= items.length; i++)
+        {
+            if (items[i] == null)
+            {
+                items[i] = _item;
+                string response = _item.name + " has been added in slot " + i;
+            }
+        }
     }
 }
